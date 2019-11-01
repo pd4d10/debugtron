e161759c1242d72284e9cb207a31565f42690f03
Rongjian Zhang <pd4d10@gmail.com>
2019-11-01 21:26:37 +0800

    style: prettier format

--- src/main/linux.ts
+++ src/main/linux.ts
@@ -2,16 +2,16 @@ import path from 'path'
 import { Adapter } from './adapter'
 
 export class LinuxAdapter extends Adapter {
-  async readApps() {
-    // TODO:
-    return []
-  }
-  async readAppByPath(p: string) {
-    return {
-      id: p,
-      name: path.basename(p),
-      icon: '',
-      exePath: p,
-    }
-  }
+  async readApps() {
+    // TODO:
+    return []
+  }
+  async readAppByPath(p: string) {
+    return {
+      id: p,
+      name: path.basename(p),
+      icon: '',
+      exePath: p,
+    }
+  }
 }
