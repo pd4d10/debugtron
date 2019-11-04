b95fd11beed6c7f96e949cc7096b460dc1a90e68
Rongjian Zhang <pd4d10@gmail.com>
2019-11-04 14:56:46 +0800

    feat: read deb apps

--- src/main/linux.ts
+++ src/main/linux.ts
@@ -1,10 +1,22 @@
 import path from 'path'
+import ini from 'ini'
+import fs from 'fs'
 import { Adapter } from './adapter'
+import { readdirSafe, readFileSafe } from './utils'
+import { AppInfo } from '../types'
+
+const desktopFilesDir = '/usr/share/applications'
 
 export class LinuxAdapter extends Adapter {
   async readApps() {
-    // TODO:
-    return []
+    const files = await readdirSafe(desktopFilesDir)
+    const apps = await Promise.all(
+      files.map(file => {
+        if (!file.endsWith('.desktop')) return
+        return this.readAppInfo(path.join(desktopFilesDir, file))
+      }),
+    )
+    return apps
   }
   async readAppByPath(p: string) {
     return {
@@ -12,6 +24,36 @@ export class LinuxAdapter extends Adapter {
       name: path.basename(p),
       icon: '',
       exePath: p,
+    }
+  }
+
+  private async readAppInfo(desktopFile: string): Promise<AppInfo | undefined> {
+    const content = await readFileSafe(desktopFile)
+
+    const entry = ini.parse(content)['Desktop Entry'] as {
+      Name?: string
+      Icon?: string
+      Exec?: string
+    }
+    if (!entry || !entry.Exec) return
+
+    let exePath = ''
+    if (entry.Exec.startsWith('"')) {
+      exePath = entry.Exec.replace(/^"(.*)".*/, '$1')
+    } else {
+      // Remove arg
+      exePath = entry.Exec.split(/\s+/)[0]
+    }
+
+    if (!exePath.startsWith('/')) return
+
+    if (!fs.existsSync(path.join(exePath, '../resources/electron.asar'))) return
+
+    return {
+      id: exePath,
+      icon: '', // TODO: Read icon
+      name: entry.Name || path.basename(exePath),
+      exePath: exePath,
     }
   }
 }
