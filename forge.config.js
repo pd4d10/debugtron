a23be5a30fa724c7e2e4e52cb45664259475eb93
Rongjian Zhang <pd4d10@gmail.com>
2024-05-03 16:59:04 +0800

    build: update vite configs from template

--- forge.config.js
+++ forge.config.js
@@ -16,13 +16,13 @@ module.exports = {
       name: "@electron-forge/plugin-vite",
       config: {
         build: [
-          { entry: "src/main.ts", config: "vite.main.config.ts" },
+          { entry: "src/main.ts", config: "vite.main.config.mjs" },
           {
             entry: "src/preload.ts",
-            // config: 'vite.preload.config.ts',
+            config: "vite.preload.config.mjs",
           },
         ],
-        renderer: [{ name: "main_window", config: "vite.renderer.config.ts" }],
+        renderer: [{ name: "main_window", config: "vite.renderer.config.mjs" }],
       },
     },
     { name: "@electron-forge/plugin-auto-unpack-natives", config: {} },
---
+++ vite.main.config.mjs
@@ -0,0 +1,35 @@
+import {
+  getBuildConfig,
+  getBuildDefine,
+  external,
+  pluginHotRestart,
+} from "./vite.base.config.mjs";
+import { defineConfig, mergeConfig } from "vite";
+
+// https://vitejs.dev/config
+export default defineConfig((env) => {
+  /** @type {import('vite').ConfigEnv<'build'>} */
+  const forgeEnv = env;
+  const { forgeConfigSelf } = forgeEnv;
+  const define = getBuildDefine(forgeEnv);
+  const config = {
+    build: {
+      lib: {
+        entry: forgeConfigSelf.entry,
+        fileName: () => "[name].js",
+        formats: ["cjs"],
+      },
+      rollupOptions: {
+        external,
+      },
+    },
+    plugins: [pluginHotRestart("restart")],
+    define,
+    resolve: {
+      // Load the Node.js entry.
+      mainFields: ["module", "jsnext:main", "jsnext"],
+    },
+  };
+
+  return mergeConfig(getBuildConfig(forgeEnv), config);
+});
