2ea7ec3dcf1ffd7ea9fcb58a437db9a89709998c
Rongjian Zhang <pd4d10@gmail.com>
2024-05-03 18:42:15 +0800

    chore: add sourcemap for debug

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -12,8 +12,11 @@ export default defineConfig((env) => {
   const forgeEnv = env;
   const { forgeConfigSelf } = forgeEnv;
   const define = getBuildDefine(forgeEnv);
+
+  /** @type {import('vite').UserConfig} */
   const config = {
     build: {
+      sourcemap: true,
       lib: {
         entry: forgeConfigSelf.entry,
         fileName: () => "[name].js",
