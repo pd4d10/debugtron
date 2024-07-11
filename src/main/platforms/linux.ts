9082d8716274b1acd2eb7a186f7e112154d42959
Rongjian Zhang <pd4d10@gmail.com>
2024-07-12 02:02:39 +0800

    style: format

--- src/main/platforms/linux.ts
+++ src/main/platforms/linux.ts
@@ -1,13 +1,13 @@
+import { findExecPath } from "./linux/find-exec";
+import { findIconPath } from "./linux/find-icon";
 import type { AppReader } from "./utils";
-import os from 'os'
 import fs from "fs";
 import ini from "ini";
+import os from "os";
 import path from "path";
 import { Result } from "ts-results";
-import { findIconPath } from './linux/find-icon'
-import { findExecPath } from './linux/find-exec'
 
-const userAppsDir = path.join(os.homedir(), ".local/share/applications")
+const userAppsDir = path.join(os.homedir(), ".local/share/applications");
 const sysAppsDir = "/usr/share/applications";
 
 const readAppInfo = (desktopFile: string) =>
@@ -35,7 +35,7 @@ const readAppInfo = (desktopFile: string) =>
 
     // try to find real exec file
     if (!path.isAbsolute(exePath)) {
-      exePath = findExecPath(exePath) || exePath
+      exePath = findExecPath(exePath) || exePath;
     }
 
     if (!path.isAbsolute(exePath)) {
@@ -44,10 +44,8 @@ const readAppInfo = (desktopFile: string) =>
 
     if (
       !(
-        fs.existsSync(path.join(exePath, "../resources/electron.asar"))
-        ||
-        fs.existsSync(path.join(exePath, "../LICENSE.electron.txt"))
-        ||
+        fs.existsSync(path.join(exePath, "../resources/electron.asar")) ||
+        fs.existsSync(path.join(exePath, "../LICENSE.electron.txt")) ||
         fs.existsSync(path.join(exePath, "../chrome-sandbox"))
       )
     ) {
@@ -58,20 +56,20 @@ const readAppInfo = (desktopFile: string) =>
     let iconPath = ""; // todo: set default icon
     if (entry.Icon) {
       if (path.isAbsolute(entry.Icon)) {
-        iconPath = entry.Icon
+        iconPath = entry.Icon;
       } else {
-        iconPath = findIconPath(entry.Icon) || iconPath
+        iconPath = findIconPath(entry.Icon) || iconPath;
       }
 
       if (fs.existsSync(iconPath)) {
-        const base64 = await fs.promises.readFile(iconPath, 'base64')
+        const base64 = await fs.promises.readFile(iconPath, "base64");
 
-        const ext = path.extname(iconPath)
-        if (ext === '.svg') {
-          icon = 'data:image/svg+xml;base64,' + base64
+        const ext = path.extname(iconPath);
+        if (ext === ".svg") {
+          icon = "data:image/svg+xml;base64," + base64;
         }
-        if (ext === '.png') {
-          icon = 'data:image/png;base64,' + base64
+        if (ext === ".png") {
+          icon = "data:image/png;base64," + base64;
         }
         // todo: if (ext === 'xpm') {}
       }
@@ -86,20 +84,20 @@ const readAppInfo = (desktopFile: string) =>
   });
 
 async function readAppDir(dir: string) {
-    const files = await fs.promises.readdir(dir)
-    const apps = await Promise.all(
-        files
-            .filter((f) => f.endsWith(".desktop"))
-            .map((file) => readAppInfo(path.join(dir, file))),
-    );
-    return apps.flatMap((app) => (app.ok ? [app.unwrap()] : []))
+  const files = await fs.promises.readdir(dir);
+  const apps = await Promise.all(
+    files
+      .filter((f) => f.endsWith(".desktop"))
+      .map((file) => readAppInfo(path.join(dir, file))),
+  );
+  return apps.flatMap((app) => (app.ok ? [app.unwrap()] : []));
 }
 
 export const adapter: AppReader = {
   readAll: () =>
     Result.wrapAsync(async () => {
-      const userApps = await readAppDir(userAppsDir)
-      const sysApps = await readAppDir(sysAppsDir)
+      const userApps = await readAppDir(userAppsDir);
+      const sysApps = await readAppDir(sysAppsDir);
       return [...userApps, ...sysApps];
     }),
   readByPath: (p: string) =>
