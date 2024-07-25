ce755542752924fd80eb9317e931fcfc6adbc211
Rongjian Zhang <pd4d10@gmail.com>
2024-07-25 18:15:08 +0800

    fix: import path

--- src/main/store.ts
+++ src/main/store.ts
@@ -1,7 +1,7 @@
 import { appSlice } from "../reducers/app";
 import { sessionSlice } from "../reducers/session";
 import { configureStore } from "@reduxjs/toolkit";
-import { stateSyncEnhancer } from "electron-redux/types/main";
+import { stateSyncEnhancer } from "electron-redux/main";
 
 export const store = configureStore({
   reducer: {
